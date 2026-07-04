8a2f769787519d31e007336beed4a92466b8cff8
Talha Zaidi <71030605+talhazaidi13@users.noreply.github.com>
2026-07-04 15:39:39 -0500

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        return self.divide(lists, 0, len(lists) - 1)
+    def divide (self, lists, l , r):
+        if l == r:
+            return lists[l]
+        mid = l + (r - l) // 2
+        left = self.divide(lists, l, mid)
+        right = self.divide(lists, mid + 1, r)
+        return self.merge(left, right)
+    def merge(self, l1, l2):
+        dummy = ListNode(0); curr = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        if l1:
+            curr.next = l1
+        else:
+            curr.next = l2
+        return dummy.next
+
+        # res = []
+        # i = j = 0
+        # while i<len(left) and j<len(right):
+        #     if left[i] <= right[j]:
+        #         res.append(left[i])
+        #         i=i+1
+        #     else:
+        #         res.append(right[j])
+        #         j=j+1
+        
+        # res.extend(left[i:])
+        # res.extend(left[j:])
+        # return res
